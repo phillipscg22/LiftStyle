96d38c4ee4e2e37c99e3352f70ef15a51c0b6c9c
Phillips <75767057+phillipscg22@users.noreply.github.com>
2022-01-26 14:15:37 -0500

    First Sprint Push

---
+++ LiftStyle/LiftStyle/LiftStyle/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+
+namespace LiftStyle
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked_GoToViewWorkoutPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ViewWorkoutPage());
+        }
+
+        private void Button_OnClicked_GoToCreateWorkoutPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new CreateWorkoutPage());
+        }
+
+        private void Button_OnClicked_GoToWorkoutPage(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new WorkoutPage());
+        }
+
+        private void Button_OnClicked_GoToFriendZone(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FrindZonePage());
+        }
+    }
+}
