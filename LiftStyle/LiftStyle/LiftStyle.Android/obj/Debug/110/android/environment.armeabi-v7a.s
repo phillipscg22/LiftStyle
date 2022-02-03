9966ff73965a4cb626af04f356384bdb82cae7c5
ccwinfield <winfieldcc22@mail.vmi.edu>
2022-02-03 16:40:14 -0500

    Added LoginPage.xaml for friendzone

--- LiftStyle/LiftStyle/LiftStyle.Android/obj/Debug/110/android/environment.armeabi-v7a.s
+++ LiftStyle/LiftStyle/LiftStyle.Android/obj/Debug/110/android/environment.armeabi-v7a.s
@@ -99,7 +99,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"767fe5e3-169f-4275-858c-740ea203e970"
+	.asciz	"5b95169f-943a-4caf-abef-ec324f685766"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
