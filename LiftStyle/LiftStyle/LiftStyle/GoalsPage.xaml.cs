ad6acde4f01a4b8aa2b8876c9cb0ebb9ad88b32e
Phillips-PC <phillipscg22@mail.vmi.edu>
2022-03-08 19:27:30 -0500

    Started goals page

---
+++ LiftStyle/LiftStyle/LiftStyle/GoalsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LiftStyle
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GoalsPage : ContentPage
+    {
+        public GoalsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
