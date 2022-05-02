27048049c9f928f7286d4576e23ab9ca830b0cf7
Phillips-PC <phillipscg22@mail.vmi.edu>
2022-05-02 08:09:20 -0400

    last push i promise

--- LiftStyle/LiftStyle/LiftStyle/CreateWorkoutPage.xaml.cs
+++ LiftStyle/LiftStyle/LiftStyle/CreateWorkoutPage.xaml.cs
@@ -32,40 +32,64 @@ namespace LiftStyle
             try
             {
 
-                string workout = WorkoutSelect.SelectedItem.ToString() + ".txt";
-                
-                // Write the string array to a new file named "CurrentWorkout.txt".
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, workout)))
+                if (WorkoutSelect.SelectedIndex != -1)
                 {
-                    outputFile.Write(exName1.Text + " | " + exSets1.SelectedItem.ToString() + " Sets | " + exReps1.SelectedItem.ToString() + " Reps | " + exWeight1.Text + " lbs\n");
-                    outputFile.Write(exName2.Text + " | " + exSets2.SelectedItem.ToString() + " Sets | " + exReps2.SelectedItem.ToString() + " Reps | " + exWeight2.Text + " lbs\n");
-                    outputFile.Write(exName3.Text + " | " + exSets3.SelectedItem.ToString() + " Sets | " + exReps3.SelectedItem.ToString() + " Reps | " + exWeight3.Text + " lbs\n");
-                    outputFile.Write(exName4.Text + " | " + exSets4.SelectedItem.ToString() + " Sets | " + exReps4.SelectedItem.ToString() + " Reps | " + exWeight4.Text + " lbs\n");
-                    outputFile.Write(exName5.Text + " | " + exSets5.SelectedItem.ToString() + " Sets | " + exReps5.SelectedItem.ToString() + " Reps | " + exWeight5.Text + " lbs\n");
-                    outputFile.Write(exName6.Text + " | " + exSets6.SelectedItem.ToString() + " Sets | " + exReps6.SelectedItem.ToString() + " Reps | " + exWeight6.Text + " lbs\n");
-                    outputFile.Write(exName7.Text + " | " + exSets7.SelectedItem.ToString() + " Sets | " + exReps7.SelectedItem.ToString() + " Reps | " + exWeight7.Text + " lbs\n");
-                    outputFile.Write(exName8.Text + " | " + exSets8.SelectedItem.ToString() + " Sets | " + exReps8.SelectedItem.ToString() + " Reps | " + exWeight8.Text + " lbs\n");
-                    outputFile.Write(exName9.Text + " | " + exSets9.SelectedItem.ToString() + " Sets | " + exReps9.SelectedItem.ToString() + " Reps | " + exWeight9.Text + " lbs\n");
-                    outputFile.Write(exName10.Text + " | " + exSets10.SelectedItem.ToString() + " Sets | " + exReps10.SelectedItem.ToString() + " Reps | " + exWeight10.Text + " lbs\n");
+                    string workout = WorkoutSelect.SelectedItem.ToString() + ".txt";
+
+                    // Write the string array to a new file named "CurrentWorkout.txt".
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, workout)))
+                    {
+                        outputFile.Write(exName1.Text + " | " + exSets1.SelectedItem.ToString() + " Sets | " +
+                                         exReps1.SelectedItem.ToString() + " Reps | " + exWeight1.Text + " lbs\n");
+                        outputFile.Write(exName2.Text + " | " + exSets2.SelectedItem.ToString() + " Sets | " +
+                                         exReps2.SelectedItem.ToString() + " Reps | " + exWeight2.Text + " lbs\n");
+                        outputFile.Write(exName3.Text + " | " + exSets3.SelectedItem.ToString() + " Sets | " +
+                                         exReps3.SelectedItem.ToString() + " Reps | " + exWeight3.Text + " lbs\n");
+                        outputFile.Write(exName4.Text + " | " + exSets4.SelectedItem.ToString() + " Sets | " +
+                                         exReps4.SelectedItem.ToString() + " Reps | " + exWeight4.Text + " lbs\n");
+                        outputFile.Write(exName5.Text + " | " + exSets5.SelectedItem.ToString() + " Sets | " +
+                                         exReps5.SelectedItem.ToString() + " Reps | " + exWeight5.Text + " lbs\n");
+                        outputFile.Write(exName6.Text + " | " + exSets6.SelectedItem.ToString() + " Sets | " +
+                                         exReps6.SelectedItem.ToString() + " Reps | " + exWeight6.Text + " lbs\n");
+                        outputFile.Write(exName7.Text + " | " + exSets7.SelectedItem.ToString() + " Sets | " +
+                                         exReps7.SelectedItem.ToString() + " Reps | " + exWeight7.Text + " lbs\n");
+                        outputFile.Write(exName8.Text + " | " + exSets8.SelectedItem.ToString() + " Sets | " +
+                                         exReps8.SelectedItem.ToString() + " Reps | " + exWeight8.Text + " lbs\n");
+                        outputFile.Write(exName9.Text + " | " + exSets9.SelectedItem.ToString() + " Sets | " +
+                                         exReps9.SelectedItem.ToString() + " Reps | " + exWeight9.Text + " lbs\n");
+                        outputFile.Write(exName10.Text + " | " + exSets10.SelectedItem.ToString() + " Sets | " +
+                                         exReps10.SelectedItem.ToString() + " Reps | " + exWeight10.Text + " lbs\n");
+                    }
                 }
-                
-                using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "CurrentWorkout.txt")))
+                else
                 {
-                    outputFile.Write(exName1.Text + " | " + exSets1.SelectedItem.ToString() + " Sets | " + exReps1.SelectedItem.ToString() + " Reps | " + exWeight1.Text + " lbs\n");
-                    outputFile.Write(exName2.Text + " | " + exSets2.SelectedItem.ToString() + " Sets | " + exReps2.SelectedItem.ToString() + " Reps | " + exWeight2.Text + " lbs\n");
-                    outputFile.Write(exName3.Text + " | " + exSets3.SelectedItem.ToString() + " Sets | " + exReps3.SelectedItem.ToString() + " Reps | " + exWeight3.Text + " lbs\n");
-                    outputFile.Write(exName4.Text + " | " + exSets4.SelectedItem.ToString() + " Sets | " + exReps4.SelectedItem.ToString() + " Reps | " + exWeight4.Text + " lbs\n");
-                    outputFile.Write(exName5.Text + " | " + exSets5.SelectedItem.ToString() + " Sets | " + exReps5.SelectedItem.ToString() + " Reps | " + exWeight5.Text + " lbs\n");
-                    outputFile.Write(exName6.Text + " | " + exSets6.SelectedItem.ToString() + " Sets | " + exReps6.SelectedItem.ToString() + " Reps | " + exWeight6.Text + " lbs\n");
-                    outputFile.Write(exName7.Text + " | " + exSets7.SelectedItem.ToString() + " Sets | " + exReps7.SelectedItem.ToString() + " Reps | " + exWeight7.Text + " lbs\n");
-                    outputFile.Write(exName8.Text + " | " + exSets8.SelectedItem.ToString() + " Sets | " + exReps8.SelectedItem.ToString() + " Reps | " + exWeight8.Text + " lbs\n");
-                    outputFile.Write(exName9.Text + " | " + exSets9.SelectedItem.ToString() + " Sets | " + exReps9.SelectedItem.ToString() + " Reps | " + exWeight9.Text + " lbs\n");
-                    outputFile.Write(exName10.Text + " | " + exSets10.SelectedItem.ToString() + " Sets | " + exReps10.SelectedItem.ToString() + " Reps | " + exWeight10.Text + " lbs\n");
+                    using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "CurrentWorkout.txt")))
+                    {
+                        outputFile.Write(exName1.Text + " | " + exSets1.SelectedItem.ToString() + " Sets | " +
+                                         exReps1.SelectedItem.ToString() + " Reps | " + exWeight1.Text + " lbs\n");
+                        outputFile.Write(exName2.Text + " | " + exSets2.SelectedItem.ToString() + " Sets | " +
+                                         exReps2.SelectedItem.ToString() + " Reps | " + exWeight2.Text + " lbs\n");
+                        outputFile.Write(exName3.Text + " | " + exSets3.SelectedItem.ToString() + " Sets | " +
+                                         exReps3.SelectedItem.ToString() + " Reps | " + exWeight3.Text + " lbs\n");
+                        outputFile.Write(exName4.Text + " | " + exSets4.SelectedItem.ToString() + " Sets | " +
+                                         exReps4.SelectedItem.ToString() + " Reps | " + exWeight4.Text + " lbs\n");
+                        outputFile.Write(exName5.Text + " | " + exSets5.SelectedItem.ToString() + " Sets | " +
+                                         exReps5.SelectedItem.ToString() + " Reps | " + exWeight5.Text + " lbs\n");
+                        outputFile.Write(exName6.Text + " | " + exSets6.SelectedItem.ToString() + " Sets | " +
+                                         exReps6.SelectedItem.ToString() + " Reps | " + exWeight6.Text + " lbs\n");
+                        outputFile.Write(exName7.Text + " | " + exSets7.SelectedItem.ToString() + " Sets | " +
+                                         exReps7.SelectedItem.ToString() + " Reps | " + exWeight7.Text + " lbs\n");
+                        outputFile.Write(exName8.Text + " | " + exSets8.SelectedItem.ToString() + " Sets | " +
+                                         exReps8.SelectedItem.ToString() + " Reps | " + exWeight8.Text + " lbs\n");
+                        outputFile.Write(exName9.Text + " | " + exSets9.SelectedItem.ToString() + " Sets | " +
+                                         exReps9.SelectedItem.ToString() + " Reps | " + exWeight9.Text + " lbs\n");
+                        outputFile.Write(exName10.Text + " | " + exSets10.SelectedItem.ToString() + " Sets | " +
+                                         exReps10.SelectedItem.ToString() + " Reps | " + exWeight10.Text + " lbs\n");
+                    }
                 }
 
-               
             }
-            catch (Exception ignored)
+            catch (Exception er)
             {
             }
             DisplayAlert("Great Success","Go to the View Workout page to view your masterful creation","Got it!" );
